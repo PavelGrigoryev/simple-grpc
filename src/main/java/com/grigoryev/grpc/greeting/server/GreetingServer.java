f0d3a6fff85330276de498a20038005c0f5765ab
Grigoryev Pavel <Green-ono@yandex.ru>
2023-10-08 18:22:09 +0300

    add string '2' to GreetingServer

--- src/main/java/com/grigoryev/grpc/greeting/server/GreetingServer.java
+++ src/main/java/com/grigoryev/grpc/greeting/server/GreetingServer.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class GreetingServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        log.info("Hello ice gRPC)");
+        log.info("Hello ice2 gRPC)");
 
         Server server = ServerBuilder.forPort(50051)
                 .addService(new GreetServiceImpl())
